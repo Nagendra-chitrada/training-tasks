6094a8d46a7ba0b649abb5f7023e1ab8d90f38be
Nagendra-chitrada <nagendra.chitrada@feuji.com>
2022-12-06 17:31:59 +0530

    modified forkify

---
+++ forkify-webpage/forkifyjs2.js
@@ -0,0 +1,61 @@
+var btnPrevioues=document.querySelector(".btn-left");
+var btnNext=document.querySelector(".btn-right");
+var resultSider=document.querySelector("#searchresults");
+var i=0,count=0;
+//----------------------common function------------------------------//
+function results(){
+  
+    
+        resultSider.innerHTML+=`
+        <li>
+        <div class="forkify-resultsbar">
+        <div class="result-img"><img src="${itemsForkify[i].image_url}"></div>
+        <h1>${itemsForkify[i].title}</h1><br>
+        <p>${itemsForkify[i].publisher} </p>
+        </div>
+        </li>
+        `;
+    count++;
+    console.log(i);
+    }
+    
+
+
+//-------------------------------------------------------------------//
+
+        
+
+//-------button Next----//
+btnNext.addEventListener("click",nextPage);
+
+function nextPage(){
+    count=0;
+    resultSider.innerHTML="";
+    while(i<itemsForkify.length){
+        if(count!=10){
+    results();
+        btnNext.disabled=(i==itemsForkify.length-1)?true:false;
+        i++;
+        }else{
+            break;
+        }
+
+    }
+
+}
+//--------button Back----//
+btnPrevioues.addEventListener("click",backPage);
+
+function backPage(){
+    i-=10;
+    while(i<itemsForkify.length){
+        if(count!=10){
+    results();
+        btnNext.disabled=(i==itemsForkify.length-1)?true:false;
+        i++;
+        }else{
+            break;
+        }
+
+    }
+}
