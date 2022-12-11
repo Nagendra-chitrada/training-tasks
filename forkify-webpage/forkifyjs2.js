0bef60d7b7a229549a1bcf1d3095e65c0668866a
Nagendra-chitrada <nagendra.chitrada@feuji.com>
2022-12-11 20:03:36 +0530

    forkify with 1 stage of api

--- forkify-webpage/forkifyjs2.js
+++ forkify-webpage/forkifyjs2.js
@@ -1,62 +1,66 @@
-/*
+var btnSearch=document.getElementById("btn-search");
 var btnPrevioues=document.querySelector(".btn-left");
 var btnNext=document.querySelector(".btn-right");
 var resultSider=document.querySelector("#searchresults");
 var i=0,count=0;
-//----------------------common function------------------------------//
-function results(){
-  
-    
-        resultSider.innerHTML+=`
-        <li>
-        <div class="forkify-resultsbar">
-        <div class="result-img"><img src="${itemsForkify[i].image_url}"></div>
-        <h1>${itemsForkify[i].title}</h1><br>
-        <p>${itemsForkify[i].publisher} </p>
-        </div>
-        </li>
-        `;
-    count++;
-    console.log(i);
-    }
-    
+var searchRecipe,
+	data,
+	recipesResult,
+	recipeData,
+	ingredientsList;
 
-
-//-------------------------------------------------------------------//
-
-        
-
-//-------button Next----//
-btnNext.addEventListener("click",nextPage);
-
-function nextPage(){
-    count=0;
-    resultSider.innerHTML="";
-    while(i<itemsForkify.length){
-        if(count!=10){
-        results();
-        btnNext.disabled=(i==itemsForkify.length-1)?true:false;
-        i++;
-        }else{
-            break;
-        }
-
-    }
+console.log(btnSearch);
+ 
+ btnSearch.addEventListener('click',searching);
+ 
+ //------function to FETCH data from API
+ 
+async function searching(searchRecipe){
+ searchRecipe=document.getElementById('search-recipe').value;
+ data=await fetch(`https://forkify-api.herokuapp.com/api/search?q=${searchRecipe}`);
+ recipesResult= await data.json();
+ console.log(recipesResult);
+ nextPage();
+ 	btnPrevioues.addEventListener("click",prevousPage);// button to go backpage of recipiesList
+ 	btnNext.addEventListener("click",nextPage);	  //button to go next page of recipiesList
 
 }
-//--------button Back----//
-btnPrevioues.addEventListener("click",backPage);
 
-function backPage(){
-    i-=10;
-    while(i<itemsForkify.length){
-        if(count!=10){
-    results();
-        btnNext.disabled=(i==itemsForkify.length-1)?true:false;
-        i++;
-        }else{
-            break;
-        }
+//----------function to go next page of recipes
+ function nextPage(){
+count++;
+  i=(count*10)-10;
+  display();
+}
 
-    }
-}*/
+//----------function to go back page 
+function prevousPage(){	
+	count--;
+ display(); 	
+}
+//----------function to display the list of Recipes
+function display(){
+	i=(count*10)-10;
+  	resultSider.innerHTML='';
+  	//loop to display the recipies in the left-side-bar 
+	while(i<(count*10)){
+	resultSider.innerHTML+=`
+	<li>
+	<div class="forkify-resultsbar" onclick="recipeDetails(${recipesResult.recipes[i].recipe_id})">
+		<div class="result-img"><img src="${recipesResult.recipes[i].image_url}"></div>
+		  <h1>${recipesResult.recipes[i].title}</h1><br>
+		<p>${recipesResult.recipes[i].publisher} </p>
+	</div>
+	</li>`;
+	i++;
+}
+}
+//----------function to display the details of selected recipie
+async function recipeDetails(id){
+	console.log(id);
+	recipeData=await fetch(`https://forkify-api.herokuapp.com/api/get?rId=${id}`);
+	ingredientsList=await recipeData.json();
+	console.log(ingredientsList);
+	
+	
+}
